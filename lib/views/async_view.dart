8632248972e0cf71679b49fab1b9bacf8c335a1b
Aru <aruinarshad@gmail.com>
2020-07-30 13:58:52 +0530

    Created a Map and displayin Live Location

--- lib/views/async_view.dart
+++ lib/views/async_view.dart
@@ -3,6 +3,8 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:location_service/datamodels/user_location.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 class ResultsPage extends StatefulWidget {
   @override
@@ -28,12 +30,31 @@ class _ResultsPageState extends State<ResultsPage> {
         Widget widget;
         if (snapshot.hasData) {
           widget = Scaffold(
-              body: Center(
-                  child: Text(
-            'Lat: ${snapshot.data[0]} Long: ${snapshot.data[1]}',
-            style: TextStyle(
-                color: Colors.red, fontSize: 18, fontWeight: FontWeight.bold),
-          )));
+              body: FlutterMap(
+            options: new MapOptions(
+              center: new LatLng(7.2906, 80.6337),
+              zoom: 7.5,
+            ),
+            layers: [
+              new TileLayerOptions(
+                  urlTemplate:
+                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: ['a', 'b', 'c']),
+              new MarkerLayerOptions(
+                markers: [
+                  new Marker(
+                    width: 40.0,
+                    height: 40.0,
+                    point: new LatLng(double.parse(snapshot.data[0]),
+                        double.parse(snapshot.data[1])),
+                    builder: (ctx) => new Container(
+                      child: Image.asset("assets/images/marker.png"),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ));
         } else {
           widget = Container(
             color: Colors.white,
@@ -47,3 +68,11 @@ class _ResultsPageState extends State<ResultsPage> {
     );
   }
 }
+
+//Center(
+//child: Text(
+//'Lat: ${snapshot.data[0]} Long: ${snapshot.data[1]}',
+//style: TextStyle(
+//color: Colors.red, fontSize: 18, fontWeight: FontWeight.bold),
+//)
+//)
